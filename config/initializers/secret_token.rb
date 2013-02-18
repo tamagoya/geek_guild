b31dcd75d4408c124617d9fad3d9ca3679030ad2
Syunsuke Mihara <mihara.git@gmail.com>
2013-02-19 02:23:03 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeekGuild::Application.config.secret_token = '5ea002837b34afd24e46fe286e03fb28ecc2e74796c2f676993e6fcd4edd43731dda763f85ba35d07694a7aad93902453e1ea3ac4c40e5680f292d84dc001afd'
